a326014f92e214a2bc94bd1e77921824450ad758
ashutoshpandey03 <ashutosh.pandey03@nagarro.com>
2024-03-04 12:55:20 +0530

    did some Ui changes , sweet alert mssg thing

--- Module Federation/host-app/src/app/app.component.ts
+++ Module Federation/host-app/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Routes } from '@angular/router';
+import { fromEvent } from 'rxjs';
 import { IWebWorker } from 'src/model/iweb-worker';
 import Swal from 'sweetalert2';
 
@@ -22,54 +23,18 @@ export class AppComponent implements OnInit {
   saveLog: boolean = true;
 
   ngOnInit(): void {
-    window.addEventListener(
-      'message',
-      this.receivePremiumNotificationMFE1.bind(this)
-    );
+    fromEvent(window, 'event').subscribe((event: any) => {
+      this.showBannerMessage('Request Sent.');
+    });
+    fromEvent(window, 'customEvent').subscribe((event: any) => {
+      this.showBannerMessage('Emi Paid Successfully.');
+    });
   }
 
   showLegends() {
     this.showLegendFlag = !this.showLegendFlag;
   }
 
-  constructor(private http: HttpClient) {}
-
-  receivePremiumNotificationMFE1(event: MessageEvent) {
-    if (event.data.id) {
-      window.addEventListener(
-        'message',
-        this.receivePaidPremiumfromMFE2.bind(this)
-      );
-      if (this.changeBannerMessage) {
-        this.showBannerMessage('Request Sent.');
-        this.changeBannerMessage = !this.changeBannerMessage;
-      }
-      this.info = this.storeInsuranceInfo(event.data);
-    }
-  }
-
-  receivePaidPremiumfromMFE2(event: MessageEvent) {
-    if (event.data.id) {
-      if (!this.changeBannerMessage) {
-        this.showBannerMessage('Emi Paid Successfully.');
-        this.changeBannerMessage = !this.changeBannerMessage;
-      }
-      const worker = new Worker(
-        new URL('src/web_worker/web-worker.worker.ts', import.meta.url)
-      );
-      worker.onmessage = ({ data }) => {};
-      worker.postMessage(this.generateLogData());
-
-      worker.addEventListener('message', (event) => {
-        if (this.saveLog) {
-          this.http
-            .post('http://localhost:3000/savedLogs', this.generateLogData())
-            .subscribe((response) => {});
-          this.saveLog = !this.saveLog;
-        }
-      });
-    }
-  }
 
   private showBannerMessage(title: string) {
     const Toast = Swal.mixin({
