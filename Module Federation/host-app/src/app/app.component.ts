701fa61eefc3b48f8a4b520cca3c4e83b7e90daf
ashutoshpandey03 <ashutosh.pandey03@nagarro.com>
2024-03-04 12:59:52 +0530

    some code clean up

--- Module Federation/host-app/src/app/app.component.ts
+++ Module Federation/host-app/src/app/app.component.ts
@@ -1,8 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Routes } from '@angular/router';
 import { fromEvent } from 'rxjs';
-import { IWebWorker } from 'src/model/iweb-worker';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -11,16 +8,8 @@ import Swal from 'sweetalert2';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  appRoutes: Routes = [];
   title = 'insurance-shell';
-
-  child1Reference: any;
-  child2Reference: any;
-  textContent = '';
-  info: any;
   showLegendFlag: boolean = false;
-  changeBannerMessage: boolean = true;
-  saveLog: boolean = true;
 
   ngOnInit(): void {
     fromEvent(window, 'event').subscribe((event: any) => {
@@ -34,7 +23,6 @@ export class AppComponent implements OnInit {
   showLegends() {
     this.showLegendFlag = !this.showLegendFlag;
   }
-
 
   private showBannerMessage(title: string) {
     const Toast = Swal.mixin({
@@ -52,24 +40,5 @@ export class AppComponent implements OnInit {
       icon: 'success',
       title: title,
     });
-  }
-
-  private storeInsuranceInfo(insuranceInfo: any) {
-    const info = {
-      name: insuranceInfo.name,
-      emiAmount: insuranceInfo.emiValue,
-    };
-    return info;
-  }
-
-  private generateLogData(): any {
-    const logInfo: IWebWorker = {
-      name: this.info.name,
-      emiAmount: this.info.emiAmount,
-      date: new Date().toDateString(),
-      time: new Date().toLocaleTimeString(),
-      status: 'SUCCESS',
-    };
-    return logInfo;
   }
 }
